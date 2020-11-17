1281c0b5febf8c209516560c9e16296ad370241f
lukaszmichalik98 <lukasz.michalikalm@gmail.com>
2020-11-17 22:01:02 +0100

    Spring Boot Init commit

---
+++ Wortal/src/main/kotlin/com/okta/Wortal/WortalApplication.kt
@@ -0,0 +1,11 @@
+package com.okta.Wortal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WortalApplication
+
+fun main(args: Array<String>) {
+	runApplication<WortalApplication>(*args)
+}
