046f0d96681aa9422f0e12e2f5dc68dc19214f96
lukaszmichalik98 <lukasz.michalikalm@gmail.com>
2020-11-18 15:05:56 +0100

    Fixed CORS errors.

--- Wortal/src/main/kotlin/com/okta/Wortal/WortalApplication.kt
+++ Wortal/src/main/kotlin/com/okta/Wortal/WortalApplication.kt
@@ -2,10 +2,32 @@ package com.okta.Wortal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @SpringBootApplication
 class WortalApplication
 
 fun main(args: Array<String>) {
 	runApplication<WortalApplication>(*args)
+}
+
+// Fix the CORS errors
+@Bean
+fun simpleCorsFilter(): FilterRegistrationBean<*> {
+	val source = UrlBasedCorsConfigurationSource()
+	val config = CorsConfiguration()
+	config.allowCredentials = true
+	// *** URL below needs to match the Vue client URL and port ***
+	config.allowedOrigins = listOf("http://localhost:8080")
+	config.allowedMethods = listOf("*")
+	config.allowedHeaders = listOf("*")
+	source.registerCorsConfiguration("/**", config)
+	val bean: FilterRegistrationBean<*> = FilterRegistrationBean(CorsFilter(source))
+	bean.order = Ordered.HIGHEST_PRECEDENCE
+	return bean
 }
