33288183d650a33eba0c8559df487e26e0719721
lukaszmichalik98 <lukasz.michalikalm@gmail.com>
2021-01-13 03:49:45 +0100

    Update EditProfile.vue

--- client/src/views/EditProfile.vue
+++ client/src/views/EditProfile.vue
@@ -1,185 +1,180 @@
 <template>
-  <div id="edit_profile_background">
-    <div id="edit_profile_space" />
-    <div id="edit_profile_div" class="global_div">
-      <p id="edit_profile_caption" class="global_caption">Edytuj profil</p>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">imię i nazwisko:</p>
-        <input
-          id="edit_profile_name"
-          class="global_login_or_register_data_input"
-          type="text"
-          name="name"
-          :value="currentUser.name"
-        />
-      </v-row>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">pozycja:</p>
-        <input
-          id="edit_profile_position"
-          class="global_login_or_register_data_input"
-          type="text"
-          name="position"
-          :value="currentUser.position"
-        />
-      </v-row>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">data urodzenia:</p>
-        <input
-          id="edit_profile_dob"
-          class="global_login_or_register_data_input"
-          type="date"
-          name="dob"
-          :value="currentUser.dob"
-        />
-      </v-row>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">adres e-mail:</p>
-        <input
-          id="edit_profile_email"
-          class="global_login_or_register_data_input"
-          type="email"
-          name="email"
-          :value="currentUser.email"
-        />
-      </v-row>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">login:</p>
-        <input
-          id="edit_profile_login"
-          class="global_login_or_register_data_input"
-          type="text"
-          name="login"
-          :value="currentUser.username"
-        />
-      </v-row>
-
-      <v-row id="edit_profile_row">
-        <p id="edit_profile_label">hasło:</p>
-        <input
-          id="edit_profile_password"
-          class="global_login_or_register_data_input"
-          type="password"
-          placeholder="podaj nowe hasło"
-          name="password"
-        />
-      </v-row>
-
-      <br />
-      <div id="edit_profile_buttons_div" class="global_div_centerize">
-        <v-btn id="edit_profile_button_ok" class="global_v_btn">OK</v-btn>
-        <br />
-        <v-btn
-          id="edit_profile_button_cancel"
-          class="global_v_btn"
-          to="/UserProfile"
-          >ANULUJ</v-btn
-        >
-      </div>
-    </div>
-  </div>
-</template>
-
-
-
-
-
-<script>
-export default {
-  name: 'UserProfile',
-  computed: {
-    currentUser() {
-      return this.$store.state.auth.user;
-    },
-  },
-  mounted() {
-    if (!this.currentUser) {
-      this.$router.push('/Login');
-    }
-  },
-};
-</script>
-
-
-
-
-
-<style>
-@import '../styles/style_edit_profile.css';
-</style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<template>
-  <v-app id="user_profile_app">
-    <div id="user_profile_div" class="global_div">
-      <v-col>
-        <p id="user_profile_caption" class="global_caption">
-          Profil użytkownika
-        </p>
-
-        <v-row id="user_profile_row">
-          <p id="user_profile_label">Imię i Nazwisko:</p>
-          <input
-            class="global_login_or_register_data_input"
-            v-validate="'required|min:3|max:20'"
-            type="text"
-            name="name"
-            :value="currentUser.name"
-          />
+  <v-app id="user_edit_app">
+    <div id="user_edit_div" class="user_edit_div">
+      <form name="editForm" @submit.prevent="handleEdit">
+        <v-row flex>
+          <v-col>
+            <p class="user_edit_caption">
+              Profil użytkownika
+            </p>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Imię i Nazwisko:</p>
+              <input
+                required
+                class="user_edit_input"
+                v-validate="'required|min:3|max:40'"
+                type="text"
+                name="name"
+                v-model="user.name"
+              />
+            </v-row>
+  
+            <v-row  class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Pozycja:</p>
+              <select
+                required
+                v-model="user.position"
+                
+                class="user_edit_input"
+                name="position"
+              >
+                <option value="napastnik">napastnik</option>
+                <option value="obrońca">obrońca</option>
+                <option value="bramkarz">bramkarz</option>
+                <option value="pomocnik">pomocnik</option>
+              </select>
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Data Urodzenia:</p>
+  
+              <input
+                required
+                class="user_edit_input"
+                type="date"
+                v-model="user.dob"
+                v-validate="'required'"
+                name="dob"
+              />
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Email:</p>
+  
+              <input
+                class="user_edit_input"
+                type="email"
+                v-model="user.email"
+                v-validate="'required|email|max:30'"
+                name="email"
+              />
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Username:</p>
+  
+              <input
+                class="user_edit_input"
+                type="text"
+                v-model="user.username"
+                v-validate="'required|min:3|max:20'"
+                name="username"
+              />
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Nowe Hasło:</p>
+  
+              <input
+                class="user_edit_input"
+                type="password"
+                placeholder="Wprowadź nowe hasło"
+                v-model="user.password"
+                v-validate="'min:6|max:30'"
+                name="password"
+              />
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="space-between">
+              <p class="user_edit_label">Potwierdź Hasło:</p>
+  
+              <input
+                class="user_edit_input"
+                type="password"
+                placeholder="Potwierdź nowe hasło"
+                v-validate="'min:6|max:30'"
+                v-model="confirmPassword"
+                name="confirmPassword"
+              />
+            </v-row>
+  
+            <v-row class="user_edit_row" justify="center">
+              <div v-if="message" class="alert-danger">
+                {{ message }}
+              </div>
+            </v-row>
+  
+            <v-row id="user_edit_row_button" justify="space-around">
+              
+                <v-btn color="primary" class="mt-5" large dark @click="cancel"> Anuluj </v-btn>
+              
+                <v-btn color="primary" class="mt-5" large dark type="submit">
+                  Zatwierdź
+                </v-btn>
+              
+            </v-row>
+          </v-col>
         </v-row>
-      </v-col>
+      </form>
     </div>
   </v-app>
 </template>
 
-
-
-
-
 <script>
+import UserService from '../services/user.service';
+import AuthService from '../services/auth.service';
+import User from '../models/user';
 export default {
-  name: 'UserProfile',
+  name: 'EditProfile',
+  data() {
+    return {
+      user: new User('', '', '', '', '', '', ''),
+      confirmPassword: '',
+      submitted: false,
+      successful: false,
+      message: '',
+      userValue: JSON.parse(localStorage.getItem('user')) || '',
+    };
+  },
   computed: {
     currentUser() {
-      return this.$store.state.auth.user;
+      return this.userValue;
+    },
+  },
+  methods: {
+    cancel() {
+      this.$router.push('/userProfile');
+    },
+    handleEdit() {
+      if (this.user.password == this.confirmPassword) {
+        console.log('it went to api');
+        this.user.id = this.currentUser.id;
+        UserService.editUser(this.user);
+
+        if (this.user.username != this.currentUser.username) {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login');
+        }
+      } else {
+        this.message = 'Nie pasujące hasła';
+      }
     },
   },
   mounted() {
+    this.user.name = this.currentUser.name;
+    this.user.position = this.currentUser.position;
+    this.user.dob = this.currentUser.dob;
+    this.user.email = this.currentUser.email;
+    this.user.username = this.currentUser.username;
+
     if (!this.currentUser) {
-      this.$router.push('/Login');
+      this.$router.push('/login');
     }
   },
 };
 </script>
 
-
-
-
-
 <style>
-@import '../styles/style_user_profile.css';
-@import '../styles/style_register.css';
+@import '../styles/style_user_edit.css';
 </style>
-!-->
